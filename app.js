d06767bfa0ae00b48395b278859017730e474cdc
caprosset <capucinerosset@gmail.com>
2020-07-27 12:11:23 +0200

    Project setup with signup and login

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 require('dotenv').config();
 
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
 const bodyParser = require('body-parser');
 const favicon = require('serve-favicon');
 const hbs = require('hbs');
 const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
 
+// Conexión a la BD
 mongoose
   .connect('mongodb://localhost/uber-for-loundry', {
     useNewUrlParser: true,
@@ -25,17 +28,18 @@ mongoose
     console.error('Error connecting to mongo', err);
   });
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const authRouter = require('./routes/auth');
+const usersRouter = require('./routes/users');
 
-var app = express();
+const app = express();
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
 // Middleware Setup
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -47,8 +51,35 @@ app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+
+// Session middleware
+app.use(session({
+  secret: 'never do your own laundry again',
+  resave: true, 
+  saveUninitialized: true, 
+  cookie: { maxAge: 60000 }, // tiempo de expiracion de la cookie
+  store: new MongoStore({
+    mongooseConnection: mongoose.connection,
+    ttl: 24 * 60 * 60 // time to live: 1 day
+  })
+}));
+
+app.use((req, res, next) => {
+  if (req.session.currentUser) {
+    res.locals.currentUserInfo = req.session.currentUser; // la información del usuario de la sesión (solo disponible si ha iniciado sesión)
+    res.locals.isUserLoggedIn = true; // indica que hay un usuario conectado
+  } else {
+    res.locals.isUserLoggedIn = false; // indica que NO hay un usuario conectado
+  }
+
+  next();
+});
+
+// Routes
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/', authRouter)
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/auth.js
@@ -0,0 +1,103 @@
+const express = require('express');
+const router = express.Router();
+
+const bcrypt = require('bcryptjs');
+const saltRound = 10;
+
+const User = require('../models/user');
+
+
+// GET /signup ==> renderiza el formulario de registro de usuario
+router.get('/signup', (req, res, next) => {
+  res.render('auth/signup', { errorMessage: '' });
+})
+
+// POST /signup ==> envía la informacion del usuario y lo crea en la base de datos
+router.post('/signup', async (req, res, next) => {
+  const { name, email, password } = req.body;
+  
+  // si el campo del email o de la contraseña están en blanco, mostrar mensaje de error
+  if(email === '' || password === '') {
+    res.render('auth/signup', { errorMessage: 'Enter both email and password to sign up.' });
+    return;
+  }
+
+  try {
+    // sino, comprobar en la BD si un usuario con este email no existe aún
+    const existingUser = await User.findOne({ email });
+    console.log('existingUser :>> ', existingUser);
+    
+    // si el usuario ya existe, mostrar mensaje de error
+    if(existingUser !== null) {
+      res.render('auth/signup', { errorMessage: `The email ${email} is already in use.`});
+      return;
+    }
+
+    // sino, encriptar contrasena
+    const salt = bcrypt.genSaltSync(saltRound);
+    const hashedPass = bcrypt.hashSync(password, salt);
+
+    // crear una nueva instancia del model User, con la contraseña encriptada
+    const newUser = {
+      name,
+      email, 
+      password: hashedPass
+    }
+    const userToCreate = new User(newUser); 
+
+    // ... y guardarla en la BD con el metodo save() de mongoose
+    await userToCreate.save();
+
+    // si todo sale según lo planeado, redirigir a la home
+    res.redirect('/');
+  } 
+  catch(err) {
+    res.render('auth/signup', { errorMessage: 'Something went wrong. Try again later.' });
+  }
+});
+
+
+// GET /login ==> renderiza el formulario de inicio de sesión
+router.get('/login', (req, res, next) => {
+  res.render('auth/login', { errorMessage: '' });
+});
+
+// POST /login ==> recibe los datos del usuario enviados desde el formulario e inicia sesión
+router.post('/login', async (req, res, next) => {
+  const { email, password } = req.body;
+
+  // comprobar que los campos email y password no estan vacíos, mostrar mensaje de error
+  if( email === '' || password === '' ) {
+    res.render('auth/login', { errorMessage: 'Enter both email and password to log in.' });
+    return;
+  }
+
+  try {
+    // Comprobar en la BD que el usuario existe (buscandolo por su email)
+    const foundUser = await User.findOne({email})
+
+    // si no existe usuario con este email, mostrar mensaje de error
+    if(!foundUser) {
+      res.render('auth/login', { errorMessage: `There isn't an account with email ${email}.` });
+      return;
+    }
+
+    // si existe, verificar la contraseña (usando el método compareSync()) y si no es correcta, mostrar mensaje de error
+    if (!bcrypt.compareSync(password, foundUser.password)) {
+      res.render('auth/login', { errorMessage: 'Invalid password.' });
+      return;
+    }
+
+    // si todo funciona, guardar la información del usuario en la sesión (req.session), y redirigir a la Home
+    req.session.currentUser = foundUser;
+    res.redirect('/');
+  } 
+  catch (error) {
+    next(error);
+  }
+})
+
+
+
+module.exports = router;
+
