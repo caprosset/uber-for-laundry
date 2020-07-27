f190663bf9c3a1e2624bf8736e8ddbc98d3e4436
caprosset <capucinerosset@gmail.com>
2020-07-27 19:09:09 +0200

    Delete users router and edit app.js

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
-const bodyParser = require('body-parser');
 const favicon = require('serve-favicon');
 const hbs = require('hbs');
 const mongoose = require('mongoose');
@@ -28,19 +27,18 @@ mongoose
     console.error('Error connecting to mongo', err);
   });
 
-  const indexRouter = require('./routes/index');
-  const usersRouter = require('./routes/users');
-  const authRouter = require('./routes/auth');
+const indexRouter = require('./routes/index');
+const authRouter = require('./routes/auth');
 const laundryRouter = require('./routes/laundry'); 
 
 const app = express();
 
 
-// view engine setup
+// Configuración del view engine (handlebas)
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
-// Middleware Setup
+// Otros middlewares
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -49,53 +47,49 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
 
-
-// Session middleware
+// Middleware de configuración de la sesión
 app.use(session({
   secret: 'never do your own laundry again',
   resave: true, 
   saveUninitialized: true, 
-  cookie: { maxAge: 60000 }, // tiempo de expiracion de la cookie
+  cookie: { maxAge: 60000 }, // tiempo de expiración de la cookie
   store: new MongoStore({
     mongooseConnection: mongoose.connection,
-    ttl: 24 * 60 * 60 // time to live: 1 day
+    ttl: 24 * 60 * 60 // "time to live": 1 day
   })
 }));
 
+// 
 app.use((req, res, next) => {
   if (req.session.currentUser) {
-    res.locals.currentUserInfo = req.session.currentUser; // la información del usuario de la sesión (solo disponible si ha iniciado sesión)
-    res.locals.isUserLoggedIn = true; // indica que hay un usuario conectado
+    res.locals.currentUserInfo = req.session.currentUser; // contiene la información del usuario de la sesión (solo disponible si ha iniciado sesión)
+    res.locals.isUserLoggedIn = true; // indica  que hay un usuario conectado
   } else {
-    res.locals.isUserLoggedIn = false; // indica que NO hay un usuario conectado
+    res.locals.isUserLoggedIn = false; // indica que no hay ningún usuario conectado
   }
 
   next();
 });
 
-// Routes
+// Routas
 app.use('/', indexRouter); 
-app.use('/users', usersRouter);
 app.use('/', authRouter);
 app.use('/', laundryRouter); 
 
 
-
-// catch 404 and forward to error handler
+// atrapa el error 404 y lo transfiere al middleware de manejo de errores
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
+// manejo de errores
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+  // setea las locales, provee error solo en entorno de desarrollo
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
+  // renderiza la página de error
   res.status(err.status || 500);
   res.render('error');
 });
